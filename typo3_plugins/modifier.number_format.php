90a1448d3820bacb0402c62896b0d8a0b81f68a3
stu <stu@bdc85c05-2a1c-0410-a950-9d575e21bd58>
2009-11-06 17:53:48 +0000

    [CHANGED] Default thousands for number_format
    git-svn-id: https://svn.rtpartner.ch/svn/rtpartner/Extensions/trunk/smarty@382 bdc85c05-2a1c-0410-a950-9d575e21bd58

--- typo3_plugins/modifier.number_format.php
+++ typo3_plugins/modifier.number_format.php
@@ -47,7 +47,7 @@
  **/
 
 
-function smarty_modifier_number_format($string, $decimals = 2, $dec_point = '.', $thousands_sep = '\'')
+function smarty_modifier_number_format($string, $decimals = 2, $dec_point = '.', $thousands_sep = '')
 {
     return number_format($string, $decimals, $dec_point, $thousands_sep);
 }
