826ec23ae60038caecfd55d26243c78e8b57bdcf
stu <stu@bdc85c05-2a1c-0410-a950-9d575e21bd58>
2009-11-10 13:33:18 +0000

    [FIXED] Clearing configuration cache would clear smarty cache
    git-svn-id: https://svn.rtpartner.ch/svn/rtpartner/Extensions/trunk/smarty@391 bdc85c05-2a1c-0410-a950-9d575e21bd58

--- hook/class.tx_smarty_cache.php
+++ hook/class.tx_smarty_cache.php
@@ -7,15 +7,19 @@ class tx_smarty_cache {
         $deletePid = t3lib_div::intval_positive($params['cacheCmd']);
 		$cacheFiles = t3lib_div::getFilesInDir(PATH_site . 'typo3temp/smarty_cache/');
 		if(!empty($cacheFiles)) {
-		    foreach($cacheFiles as $cacheFile) {
-		        if($deletePid) {
-		            if(strpos($cacheFile, $deletePid . '-') === 0) {
-		                @unlink(PATH_site . 'typo3temp/smarty_cache/' . $cacheFile);
-		            }
-		        } elseif(strpos(strtolower($cacheFile), 'index') !== 0) {
-		            @unlink(PATH_site . 'typo3temp/smarty_cache/' . $cacheFile);
-		        }
-		    }
+			if($deletePid || $params['cacheCmd'] == 'all' || $params['cacheCmd'] == 'pages') {
+		    	foreach($cacheFiles as $cacheFile) {				
+					if($deletePid) {
+						if(strpos($cacheFile, $deletePid . '-') === 0) {
+							@unlink(PATH_site . 'typo3temp/smarty_cache/' . $cacheFile);
+						}
+					} else {
+						if(strpos(strtolower($cacheFile), 'index') !== 0) {
+							@unlink(PATH_site . 'typo3temp/smarty_cache/' . $cacheFile);
+						}
+					}			
+				}
+			}
 		}
 	}
 }
