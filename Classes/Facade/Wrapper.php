fe308e1095b550e2519dad0420cd8ca63cc0918f
Simon Tuck <stu@rtp.ch>
2011-11-15 23:46:04 +0100

    [FEATURE] Finished tests for configuration class

--- Classes/Facade/Wrapper.php
+++ Classes/Facade/Wrapper.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2006-2007 Simon Tuck <stu@rtpartner.ch>, Rueegg Tuck Partner GmbH
+*  (c) 2011 Simon Tuck <stu@rtp.ch>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -23,9 +23,9 @@
 ***************************************************************/
 
 /**
- * @copyright 	2007 Rueegg Tuck Partner GmbH
- * @author 		Simon Tuck <stu@rtpartner.ch>
- * @link 		http://www.rtpartner.ch/
+ * @copyright 	(c) 2011 Simon Tuck
+ * @author 		Simon Tuck <stu@rtp.ch>
+ * @link 		http://www.rtp.ch/
  * @package 	Smarty (smarty)
  **/
 
@@ -46,19 +46,7 @@ class Tx_Smarty_Facade_Wrapper
      *
      * @var string
      */
-    protected $language_file            = null;
-
-    /**
-     * @var null
-     * @deprecated Use $language_file instead
-     */
-    private $path_to_language_file      = null;
-
-    /**
-     * @var null
-     * @deprecated Use $template_dir instead
-     */
-    private $path_to_template_directory = null;
+    public $language_file               = null;
 
     /**
      * @param array $options
@@ -87,11 +75,267 @@ class Tx_Smarty_Facade_Wrapper
      *
      * @return null|Tx_Smarty_Facade_Configuration
      */
-    private function getConfiguration()
+    public function getConfiguration()
     {
         if(is_null($this->configuration)) {
             $this->configuration = t3lib_div::makeInstance('Tx_Smarty_Facade_Configuration', $this);
         }
         return $this->configuration;
     }
+
+   /**
+    *
+    * Set language file(s)
+    *
+    * @api
+    * @param string|array $language_file language file(s)
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function setLanguageFile($language_file)
+    {
+        $this->language_file = array();
+        foreach ((array) $language_file as $k => $v) {
+            $this->language_file[$k] = rtrim($v, '/\\') . DS;
+        }
+        return $this;
+    }
+
+   /**
+    *
+    * Get language file(s)
+    *
+    * @api
+    * @param mixed $index of language file to get, null to get all
+    * @return array|null language file
+    */
+    public function getLanguageFile($index = null)
+    {
+        if ($index !== null) {
+            return isset($this->language_file[$index]) ? $this->language_file[$index] : null;
+        }
+        return (array) $this->language_file;
+    }
+
+   /**
+    *
+    * Adds language file(s)
+    *
+    * @api
+    * @param mixed $language_file
+    * @param string $key of the array element to assign
+    * @return Tx_Smarty_Facade_Wrapper current Smarty instance for chaining
+    */
+    public function addLanguageFile($language_file, $key = null)
+    {
+        // make sure we're dealing with an array
+        $this->language_file = (array) $this->language_file;
+
+        if (is_array($language_file)) {
+            foreach ($language_file as $k => $v) {
+                if (is_int($k)) {
+                    // indexes are not merged but appended
+                    $this->language_file[] = rtrim($v, '/\\') . DS;
+                } else {
+                    // string indexes are overridden
+                    $this->language_file[$k] = rtrim($v, '/\\') . DS;
+                }
+            }
+        } else {
+            // append new directory
+            $this->language_file[] = rtrim($language_file, '/\\') . DS;
+        }
+
+        $this->language_file = array_unique($this->language_file);
+        return $this;
+    }
+
+
+   /**
+    *
+    * Set template directory
+    *
+    * @api
+    * @param string|array $template_dir directory(s) of template sources
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function setTemplateDir($template_dir)
+    {
+        $template_dir = $this->getConfiguration()->getPaths($template_dir);
+        return parent::setTemplateDir($template_dir);
+    }
+
+   /**
+    *
+    * Add template directory(s)
+    *
+    * @api
+    * @param string|array $template_dir directory(s) of template sources
+    * @param string       $key          of the array element to assign the template dir to
+    * @return Smarty current Smarty instance for chaining
+    * @throws SmartyException when the given template directory is not valid
+    */
+    public function addTemplateDir($template_dir, $key=null)
+    {
+        $template_dir = $this->getConfiguration()->getPaths($template_dir);
+        return parent::addTemplateDir($template_dir, $key);
+    }
+
+   /**
+    *
+    * Set config directory
+    *
+    * @api
+    * @param string|array $config_dir directory(s) of configuration sources
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function setConfigDir($config_dir)
+    {
+        $config_dir = $this->getConfiguration()->getPaths($config_dir);
+        return parent::setConfigDir($config_dir);
+    }
+
+   /**
+    *
+    * Add config directory(s)
+    *
+    * @api
+    * @param string|array $config_dir directory(s) of config sources
+    * @param string $key of the array element to assign the config dir to
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function addConfigDir($config_dir, $key=null)
+    {
+        $config_dir = $this->getConfiguration()->getPaths($config_dir);
+        return parent::addConfigDir($config_dir, $key);
+    }
+
+   /**
+    *
+    * Set plugins directory
+    *
+    * @api
+    * @param string|array $plugins_dir directory(s) of plugins
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function setPluginsDir($plugins_dir)
+    {
+        $plugins_dir = $this->getConfiguration()->getPaths($plugins_dir);
+        return parent::setPluginsDir($plugins_dir);
+    }
+
+   /**
+    * 
+    * Adds directory of plugin files
+    *
+    * @api
+    * @param string|array $plugins_dir
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function addPluginsDir($plugins_dir)
+    {
+        $plugins_dir = $this->getConfiguration()->getPaths($plugins_dir);
+        return parent::addPluginsDir($plugins_dir);
+    }
+
+   /**
+    *
+    * Set compile directory
+    *
+    * @api
+    * @param string $compile_dir directory to store compiled templates in
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function setCompileDir($compile_dir)
+    {
+        $compile_dir = $this->getConfiguration()->getPaths($compile_dir);
+        return parent::setCompileDir($compile_dir);
+    }
+
+   /**
+    *
+    * Set cache directory
+    *
+    * @api
+    * @param string $cache_dir directory to store cached templates in
+    * @return Smarty current Smarty instance for chaining
+    */
+    public function setCacheDir($cache_dir)
+    {
+        $cache_dir = $this->getConfiguration()->getPaths($cache_dir);
+        return parent::setCacheDir($cache_dir);
+    }
+
+   /**
+    *
+    * Set the debug template
+    *
+    * @api
+    * @param string $tpl_name
+    * @return Smarty current Smarty instance for chaining
+    * @throws SmartyException if file is not readable
+    */
+    public function setDebugTemplate($tpl_name)
+    {
+        $tpl_name = $this->getConfiguration()->getPaths($tpl_name);
+        return parent::setDebugTemplate($tpl_name);
+    }
+
+    
+    /************************************************************************
+     * Deprecated methods
+     ************************************************************************/
+
+    /**
+     * @var null
+     * @deprecated Use $language_file instead
+     */
+    private $path_to_language_file      = null;
+
+    /**
+     * @var null
+     * @deprecated Use $template_dir instead
+     */
+    private $path_to_template_directory = null;
+
+    /**
+     *
+     * @api
+     * @param $path_to_template_directory
+     * @return void
+     * @deprecated Use setTemplateDir() instead
+     */
+    public function setPathToTemplateDirectory($path_to_template_directory)
+    {
+        $this->setTemplateDir($path_to_template_directory);
+    }
+    
+    /**
+     *
+     * Set the language file
+     *
+     * @api
+     * @param $path_to_language_file
+     * @return void
+     * @deprecated use setLanguageFile() instead
+     */
+    public function setPathToLanguageFile($path_to_language_file)
+    {
+        $this->setLanguageFile($path_to_language_file);
+    }
+    
+    /**
+     *
+     * Set smarty configuration variable
+     *
+     * @api
+     * @param $smartyVar
+     * @param $smartyValue
+     * @return void
+     * @deprecated use accessors instead
+     */
+	public function setSmartyVar($smartyVar, $smartyValue)
+    {
+        $method = 'set' . t3lib_div::underscoredToUpperCamelCase($smartyVar);
+        $this->{$method}($smartyValue);
+	}
 }
