6f39ed0ab4aea33d22ffb627f319a19376db2fec
Simon Tuck <stu@rtp.ch>
2011-12-17 13:16:04 +0100

    [FEATURE] LLL Plugin

--- Classes/SmartyPlugins/Common/block.LLL.php
+++ Classes/SmartyPlugins/Common/block.LLL.php
@@ -4,8 +4,8 @@
  *  Copyright notice
  *
  *
- *	Created by Simon Tuck <stu@rtpartner.ch>
- *	Copyright (c) 2006-2007, Rueegg Tuck Partner GmbH (wwww.rtpartner.ch)
+ *    Created by Simon Tuck <stu@rtpartner.ch>
+ *    Copyright (c) 2006-2007, Rueegg Tuck Partner GmbH (wwww.rtpartner.ch)
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -24,10 +24,10 @@
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  *
- *	@copyright 	2006, 2007 Rueegg Tuck Partner GmbH
- *	@author 	Simon Tuck <stu@rtpartner.ch>
- *	@link 		http://www.rtpartner.ch/
- *	@package 	Smarty (smarty)
+ *    @copyright     2006, 2007 Rueegg Tuck Partner GmbH
+ *    @author     Simon Tuck <stu@rtpartner.ch>
+ *    @link         http://www.rtpartner.ch/
+ *    @package     Smarty (smarty)
  *
  ***************************************************************/
 
@@ -42,62 +42,56 @@
  * Author:  Simon Tuck <stu@rtpartner.ch>, Rueegg Tuck Partner GmbH
  * Purpose: Translate a block of text from the current TYPO3 language library (e.g. locallang.xml)
  * Example: {LLL alt="Please enter your name" label="enter_name"}Your name{/LLL}
- * Note:	The parameter 'label' refers to the label in the xml file. If you do not provide a label
- * 			the content between the tags will be used as the label.
- * Note:	The 'alt' parameter enables you to provide an alternative text if no translation was found.
- * Note:	If the translated text contains Smarty variables it will be cycled through Smarty again!
- *			That means you can include Smarty tags in your language library
+ * Note:    The parameter 'label' refers to the label in the xml file. If you do not provide a label
+ *             the content between the tags will be used as the label.
+ * Note:    The 'alt' parameter enables you to provide an alternative text if no translation was found.
+ * Note:    If the translated text contains Smarty variables it will be cycled through Smarty again!
+ *            That means you can include Smarty tags in your language library
  * -------------------------------------------------------------
  *
  **/
 
+    // TODO: Inneficient if each translate causes a seek
+    function smarty_block_LLL($params, $content, Smarty_Internal_Template $template)
+    {
+        // Make sure params are lowercase
+        $params = array_change_key_case($params, CASE_LOWER);
 
-	function smarty_block_LLL($params, $content, &$smarty) {
-		
-		// Check for a valid FE instance (this plugin cannot be run in the backend)
-		if(!tx_smarty_div::validateTypo3Instance('FE')) {
-			$smarty->trigger_error($smarty->fePluginError);
-			return false;
-		}	
+        // Key for looking up the translation in the language file
+        $key = ($params['label']) ? $params['label'] : $content;
 
-		// Make sure params are lowercase
-		$params = array_change_key_case($params,CASE_LOWER);
+        // Exit if no key was provided
+        if(!$key) return $content;
 
-		// Key for looking up the translation in the language file
-		$key = ($params['label'])?$params['label']:$content;
+        // Get the language file and/or label information from the key
+        if(t3lib_div::isFirstPartOfStr($key, 'LLL:')) {
+            $key = substr($key, 4);
+        }
+    
+        if($parts = Tx_Smarty_Utility_Array::trimExplode(':', $key)) {
+            $key = array_pop($parts);
+            $languageFile = implode(':', $parts);
+        }
+        // $template->smarty->getLanguageFile(),
+        $languageFile = ($languageFile) ? $languageFile : $template->smarty->getLanguageFile();
 
-		// Exit if no key was provided
-		if(!$key) return;
+        // FIXED: For windows - only use relative path for readLLfile
+        $language_file = str_replace(PATH_site, '', $language_file);
+        // Call the sL method from tslib_fe to translate the label
+        $translation = $GLOBALS['TSFE']->sL('LLL:'.$language_file.':'.$key);
 
-		// Get the language file and/or label information from the key
-		if(!strcmp(strtoupper(substr($key, 0, 4)),'LLL:')) {
-		    $key = substr($key, 4);
-		}
-	
-		if($parts = t3lib_div::trimExplode(':', $key, 1)) {
-			$key = array_pop($parts);
-			$language_file = implode(':',$parts);
-		}
-		$language_file = ($language_file)?$language_file:$smarty->t3_languageFile;
-		// FIXED: For windows - only use relative path for readLLfile
-		$language_file = str_replace(PATH_site, '', $language_file);
-		// Call the sL method from tslib_fe to translate the label
-		$translation = $GLOBALS['TSFE']->sL('LLL:'.$language_file.':'.$key);
+        // Exit if no translation was found
+        if(!$translation) {
+            if(!empty($params['alt'])) return $params['alt'];
+            // Trigger an error if no alternate was available
+            $smarty->trigger_error('Translation unavailable for key "'.$key.'" in language "'.$GLOBALS['TSFE']->lang.'"');
+            return $content;
+        }
 
-		// Exit if no translation was found
-		if(!$translation) {
-			if(!empty($params['alt'])) return $params['alt'];
-			// Trigger an error if no alternate was available
-			$smarty->trigger_error('Translation unavailable for key "'.$key.'" in language "'.$GLOBALS['TSFE']->lang.'"');
-			return $content;
-		}
-
-		// If the result contains Smarty template vars run it through Smarty again
-		if (preg_match('/['.quotemeta($smarty->left_delimiter).'[^'.quotemeta($smarty->right_delimiter).']*'.quotemeta($smarty->right_delimiter).'/m', $translation)) {
-			return ($params['hsc'])?htmlspecialchars($smarty->display('string:'.$translation)):$smarty->display('string:'.$translation);
-		} else {
-			return ($params['hsc'])?htmlspecialchars($translation):$translation;
-		}
-	}
-
-?>
+        // If the result contains Smarty template vars run it through Smarty again
+        if (preg_match('/['.quotemeta($smarty->left_delimiter).'[^'.quotemeta($smarty->right_delimiter).']*'.quotemeta($smarty->right_delimiter).'/m', $translation)) {
+            return ($params['hsc'])?htmlspecialchars($smarty->display('string:'.$translation)):$smarty->display('string:'.$translation);
+        } else {
+            return ($params['hsc'])?htmlspecialchars($translation):$translation;
+        }
+    }
