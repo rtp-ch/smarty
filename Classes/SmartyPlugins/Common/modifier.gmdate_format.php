e6b3bee07e320c79405b03a89c02b10539f35ea0
Simon Tuck <stu@rtp.ch>
2011-12-17 13:14:54 +0100

    [FEATURE] Smarty plugins

--- Classes/SmartyPlugins/Common/modifier.gmdate_format.php
+++ Classes/SmartyPlugins/Common/modifier.gmdate_format.php
@@ -30,47 +30,54 @@
  *	@package 	Smarty (smarty)
  *
  ***************************************************************/
-/**
- * Include the {@link shared.make_timestamp.php} plugin
- */
-require_once $smarty->_get_plugin_filepath('shared', 'make_timestamp');
-/**
- *
- * Smarty plugin "gmdate_format"
- * -------------------------------------------------------------
- * File:    modifier.gmdate_format.php
- * Type:    modifier
- * Name:    Format
- * Version: 1.0
- * Author:  Simon Tuck <stu@rtpartner.ch>, Rueegg Tuck Partner GmbH
- * Purpose: Same as date_format but uses gmstrftime instead of strftime
- * Example: See date_format documentation
- * -------------------------------------------------------------
- *
- **/
 
-
-function smarty_modifier_gmdate_format($string, $format = '%b %e, %Y', $default_date = '')
-{
-    if ($string != '') {
-        $timestamp = smarty_make_timestamp($string);
-    } elseif ($default_date != '') {
-        $timestamp = smarty_make_timestamp($default_date);
-    } else {
-        return;
-    }
-    if (DIRECTORY_SEPARATOR == '\\') {
-        $_win_from = array('%D',       '%h', '%n', '%r',          '%R',    '%t', '%T');
-        $_win_to   = array('%m/%d/%y', '%b', "\n", '%I:%M:%S %p', '%H:%M', "\t", '%H:%M:%S');
-        if (strpos($format, '%e') !== false) {
-            $_win_from[] = '%e';
-            $_win_to[]   = sprintf('%\' 2d', date('j', $timestamp));
+    /**
+     * Smarty plugin "gmdate_format"
+     * -------------------------------------------------------------
+     * File:    modifier.gmdate_format.php
+     * Type:    modifier
+     * Name:    Format
+     * Version: 2.0
+     * Author:  Simon Tuck <stu@rtp.ch>, Rueegg Tuck Partner GmbH
+     * Purpose: Same as date_format but uses gmstrftime instead of strftime
+     * Example: See date_format documentation
+     * -------------------------------------------------------------
+     *
+     * @param $string
+     * @param string $format
+     * @param string $default_date
+     * @param string $formatter
+     * @return string
+     */
+    function smarty_modifier_gmdate_format($string, $format = SMARTY_RESOURCE_DATE_FORMAT, $default_date = '', $formatter='auto')
+    {
+        /**
+        * Include the {@link shared.make_timestamp.php} plugin
+        */
+        require_once(SMARTY_PLUGINS_DIR . 'shared.make_timestamp.php');
+        if ($string != '') {
+            $timestamp = smarty_make_timestamp($string);
+        } elseif ($default_date != '') {
+            $timestamp = smarty_make_timestamp($default_date);
+        } else {
+            return;
         }
-        if (strpos($format, '%l') !== false) {
-            $_win_from[] = '%l';
-            $_win_to[]   = sprintf('%\' 2d', date('h', $timestamp));
+        if($formatter=='strftime'||($formatter=='auto'&&strpos($format,'%')!==false)) {
+            if (DS == '\\') {
+                $_win_from = array('%D', '%h', '%n', '%r', '%R', '%t', '%T');
+                $_win_to = array('%m/%d/%y', '%b', "\n", '%I:%M:%S %p', '%H:%M', "\t", '%H:%M:%S');
+                if (strpos($format, '%e') !== false) {
+                    $_win_from[] = '%e';
+                    $_win_to[] = sprintf('%\' 2d', date('j', $timestamp));
+                }
+                if (strpos($format, '%l') !== false) {
+                    $_win_from[] = '%l';
+                    $_win_to[] = sprintf('%\' 2d', date('h', $timestamp));
+                }
+                $format = str_replace($_win_from, $_win_to, $format);
+            }
+            return gmstrftime($format, $timestamp);
+        } else {
+            return gmdate($format, $timestamp);
         }
-        $format = str_replace($_win_from, $_win_to, $format);
     }
-    return gmstrftime($format, $timestamp);
-}
