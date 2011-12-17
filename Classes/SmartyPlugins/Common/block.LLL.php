751a83182cfe1fe3519ebac24a421e0c313eb0fa
Simon Tuck <stu@rtp.ch>
2011-12-17 13:47:41 +0100

    [FEATURE] Translate plugins

--- Classes/SmartyPlugins/Common/block.LLL.php
+++ Classes/SmartyPlugins/Common/block.LLL.php
@@ -31,27 +31,31 @@
  *
  ***************************************************************/
 
-/**
- *
- * Smarty plugin "LLL"
- * -------------------------------------------------------------
- * File:    block.LLL.php
- * Type:    block
- * Name:    Translate Text
- * Version: 1.1
- * Author:  Simon Tuck <stu@rtpartner.ch>, Rueegg Tuck Partner GmbH
- * Purpose: Translate a block of text from the current TYPO3 language library (e.g. locallang.xml)
- * Example: {LLL alt="Please enter your name" label="enter_name"}Your name{/LLL}
- * Note:    The parameter 'label' refers to the label in the xml file. If you do not provide a label
- *             the content between the tags will be used as the label.
- * Note:    The 'alt' parameter enables you to provide an alternative text if no translation was found.
- * Note:    If the translated text contains Smarty variables it will be cycled through Smarty again!
- *            That means you can include Smarty tags in your language library
- * -------------------------------------------------------------
- *
- **/
-
-    // TODO: Inneficient if each translate causes a seek
+    /**
+     * Smarty plugin "LLL"
+     * -------------------------------------------------------------
+     * File:    block.LLL.php
+     * Type:    block
+     * Name:    Translate Text
+     * Version: 1.1
+     * Author:  Simon Tuck <stu@rtpartner.ch>, Rueegg Tuck Partner GmbH
+     * Purpose: Translate a block of text from the current TYPO3 language library (e.g. locallang.xml)
+     * Example: {LLL alt="Please enter your name" label="enter_name"}Your name{/LLL}
+     * Note:    The parameter 'label' refers to the label in the xml file. If you do not provide a label
+     *             the content between the tags will be used as the label.
+     * Note:    The 'alt' parameter enables you to provide an alternative text if no translation was found.
+     * Note:    If the translated text contains Smarty variables it will be cycled through Smarty again!
+     *            That means you can include Smarty tags in your language library
+     * -------------------------------------------------------------
+     *
+     * @param $params
+     * @param $content
+     * @param Smarty_Internal_Template $template
+     * @return string
+     * @todo Inneficient if each translate causes a seek, check extBase approach
+     * @todo extBase compatibility
+     * @todo run through string resource
+     */
     function smarty_block_LLL($params, $content, Smarty_Internal_Template $template)
     {
         // Make sure params are lowercase
@@ -59,39 +63,47 @@
 
         // Key for looking up the translation in the language file
         $key = ($params['label']) ? $params['label'] : $content;
+        if($key) {
 
-        // Exit if no key was provided
-        if(!$key) return $content;
+            // Gets the language file and/or label information
+            if(t3lib_div::isFirstPartOfStr($key, 'LLL:')) {
+                $key = substr($key, 4);
+            }
+            if($parts = Tx_Smarty_Utility_Array::trimExplode(':', $key)) {
+                $key = array_pop($parts);
+                $languageFile = implode(':', $parts);
+            }
+            $languageFile = ($languageFile) ? $languageFile : $template->smarty->getLanguageFile();
 
-        // Get the language file and/or label information from the key
-        if(t3lib_div::isFirstPartOfStr($key, 'LLL:')) {
-            $key = substr($key, 4);
+            // Makes sure only relative path is used for readLLfile
+            $languageFile = str_replace(t3lib_div::getIndpEnv('TYPO3_SITE_URL'), '', $languageFile);
+
+            // Calls the sL method from tslib_fe to translate the label
+            if (Tx_Smarty_Utility_Typo3::isFeInstance()) {
+                $translation = $GLOBALS['TSFE']->sL('LLL:' . $languageFile . ':' . $key);
+            } elseif (is_object($GLOBALS['LANG'])) {
+                $translation = $GLOBALS['LANG']->sL('LLL:' . $languageFile . ':' . $key);
+            }
+
+            // Sets an alternate translation if no translation was found
+            // and the "alt" parameter is available.
+            if(!$translation && isset($params['alt'])) {
+                $translation = $params['alt'];
+            }
+
+            // If the translation contains Smarty template vars run it through Smarty as s string resource
+            $lDel = preg_quote($template->smarty->getLeftDelimiter(), '%');
+            $rDel = preg_quote($template->smarty->getRightDelimiter(), '%');
+            if (preg_match('%[' . $lDel . '[^' . $rDel . ']*' . $rDel . '%m', $translation)) {
+                $translation = $template->smarty->fetch('string:' . $translation);
+            }
+
+            // Runs the translated text through htmlspecialchars if set
+            if((boolean) $params['hsc']) {
+                $translation = htmlspecialchars($translation);
+            }
         }
-    
-        if($parts = Tx_Smarty_Utility_Array::trimExplode(':', $key)) {
-            $key = array_pop($parts);
-            $languageFile = implode(':', $parts);
-        }
-        // $template->smarty->getLanguageFile(),
-        $languageFile = ($languageFile) ? $languageFile : $template->smarty->getLanguageFile();
 
-        // FIXED: For windows - only use relative path for readLLfile
-        $language_file = str_replace(PATH_site, '', $language_file);
-        // Call the sL method from tslib_fe to translate the label
-        $translation = $GLOBALS['TSFE']->sL('LLL:'.$language_file.':'.$key);
-
-        // Exit if no translation was found
-        if(!$translation) {
-            if(!empty($params['alt'])) return $params['alt'];
-            // Trigger an error if no alternate was available
-            $smarty->trigger_error('Translation unavailable for key "'.$key.'" in language "'.$GLOBALS['TSFE']->lang.'"');
-            return $content;
-        }
-
-        // If the result contains Smarty template vars run it through Smarty again
-        if (preg_match('/['.quotemeta($smarty->left_delimiter).'[^'.quotemeta($smarty->right_delimiter).']*'.quotemeta($smarty->right_delimiter).'/m', $translation)) {
-            return ($params['hsc'])?htmlspecialchars($smarty->display('string:'.$translation)):$smarty->display('string:'.$translation);
-        } else {
-            return ($params['hsc'])?htmlspecialchars($translation):$translation;
-        }
+        // Returns the original content if no translation was found
+        return trim($translation) ? $translation : $content;
     }
--- Classes/SmartyPlugins/Common/function.l10n.php
+++ Classes/SmartyPlugins/Common/function.l10n.php
@@ -33,16 +33,15 @@
 
 /**
  *
- * Smarty plugin "translate"
+ * Smarty plugin "l10n"
  * -------------------------------------------------------------
- * File:    block.translate.php
- * Type:    block
+ * File:    function.l10n.php
+ * Type:    function
  * Name:    Translate
- * Version: 2.0
- * Author:  Simon Tuck <stu@rtpartner.ch>, Rueegg Tuck Partner GmbH
+ * Version: 1.0
+ * Author:	Simon Tuck <stu@rtpartner.ch>, Rueegg Tuck Partner GmbH
  * Purpose: Translate a block of text from the current TYPO3 language library (e.g. locallang.xml)
- * Example: {translate alt="Please enter your name" label="enter_name"}Your name{/translate}
- * Note:	The Smarty "translate" tag is an alias for the "LLL" tag
+ * Example: {l10n label="enter_name"}
  * Note:	The parameter 'label' refers to the label xml file. If you do not provide a key
  * 			the content between the tags will be used as the key.
  * Note:	The 'alt' parameter enables you to provide an alternative text if no translation was found.
@@ -52,13 +51,14 @@
  *
  **/
 
-
-	function smarty_block_translate($params, $content, &$smarty) {
-		if($funcName = $smarty->getAndLoadPlugin('block','LLL')) {
-			return $funcName($params, $content, $smarty);
-		} else {
-			return ($params['alt'])?$params['alt']:$content;
-		}
-	}
-
-?>
+    /**
+     * @param $params
+     * @param Smarty_Internal_Template $template
+     * @return string
+     * @see smarty_block_LLL
+     */
+    function smarty_function_l10n($params, Smarty_Internal_Template $template)
+    {
+        Tx_Smarty_Utility_Smarty::loadPlugin($template, 'smarty_block_LLL');
+        return smarty_block_LLL($params, '', $template);
+    }
