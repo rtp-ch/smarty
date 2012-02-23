d99acf05df3b84f5dd412f530cb48c75e00b2527
Simon Tuck <stu@rtp.ch>
2012-02-23 12:09:07 +0100

    [TASK] Refactors cObj proxy, applies practices from misc. fluid view helpers

--- Classes/Core/CobjectProxy.php
+++ Classes/Core/CobjectProxy.php
@@ -1,91 +1,52 @@
 <?php
 
+/**
+ * A proxy object for plugins which rely on frontend tslib_cobj methods.
+ */
 class Tx_Smarty_Core_CobjectProxy
 {
     /**
-     * @var tslib_fe Contains a backup of the current $GLOBALS['TSFE'] if used in BE mode
-     */
-    private $tsfeBackup                 = null;
-
-    /**
-     * @var tslib_cObj
-     */
-    private $contentObject              = null;
-
-    /**
-     * @throws RuntimeException
-     */
-    public function __construct()
-    {
-        if (Tx_Smarty_Utility_Typo3::isFeInstance()) {
-            $this->setContentObject($GLOBALS['TSFE']->cObj);
-
-        } elseif (Tx_Smarty_Utility_Typo3::isBeInstance()) {
-            $this->simulateFrontendEnvironment();
-            $this->setContentObject($GLOBALS['TSFE']->cObj);
-
-        } else {
-            throw new RuntimeException('WTF');
-        }
-    }
-
-    /**
-     * Reroutes cObj method calls to the tslib_cObj instance
+     * Contains a backup of the current $GLOBALS['TSFE'].
      *
-     * @param string $method
-     * @param array $args
-     * @return mixed
+     * @var tslib_fe|null
      */
-    public final function __call($method, array $args = array())
-    {
-        if(!method_exists($this->getContentObject(), $method)) {
-            throw new BadMethodCallException('No such method');
-        }
-        return call_user_func_array(array($this->getContentObject(), $method), $args);
-    }
+    private $tsfeBackup;
 
     /**
-     * Creates a tslib_Cobj instance for backend mode
+     * Contains a backup of the current working directory.
      *
-     * @return Tx_Smarty_Core_CobjectProxy
-     * @see Tx_Extbase_Utility_FrontendSimulator::simulateFrontendEnvironment
+     * @var string|null
      */
-    protected function simulateFrontendEnvironment()
+    private $workingDirBackup;
+
+    /**
+     * Constructor creates a simulated frontend environment when
+     *
+     * @param array $data
+     * @param string $table
+     */
+    public function __construct(array $data = array(), $table = '')
     {
-        $this->tsfeBackup = isset($GLOBALS['TSFE']) ? $GLOBALS['TSFE'] : null;
-        // This somewhat hacky work around is currently needed because the cObjGetSingle()
-        // function of tslib_cObj relies on this setting
-        $GLOBALS['TSFE'] = new stdClass();
-        // Sets the $TSFE->cObjectDepthCounter in Backend mode
-        $GLOBALS['TSFE']->cObjectDepthCounter = 100;
-        $GLOBALS['TSFE']->cObj = t3lib_div::makeInstance('tslib_cObj');
-        $GLOBALS['TSFE']->cObj->start(array());
-        return $this;
+        if (!Tx_Smarty_Utility_Typo3::isFeInstance()) {
+            $this->simulateFrontendEnvironment($data, $table);
+        }
     }
 
     /**
-     * @return tslib_cObj
+     * Simulates a frontend environment for backend mode. Inspired by various hacks for simulating the frontend in
+     * Tx_Fluid_ViewHelpers_CObjectViewHelper, Tx_Fluid_ViewHelpers_ImageViewHelper,
+     * Tx_Fluid_ViewHelpers_Format_CropViewHelper, Tx_Fluid_ViewHelpers_Format_HtmlViewHelper and
+     * Tx_Extbase_Utility_FrontendSimulator (and possibly others...)
+     *
+     * @param array $data
+     * @param string $table
      */
-    protected function getContentObject()
+    protected function simulateFrontendEnvironment(array $data = array(), $table = '')
     {
-        if(!($this->contentObject instanceof tslib_cObj)) {
-            throw new UnexpectedValueException('Shadupayouface!');
-        }
-        return $this->contentObject;
-    }
-
-    /**
-     * @param tslib_cObj $contentObject
-     * @return Tx_Smarty_Core_CobjectProxy
-     * @throws InvalidArgumentException
-     */
-    protected function setContentObject(tslib_cObj $contentObject)
-    {
-        if(!($contentObject instanceof tslib_cObj)) {
-            throw new InvalidArgumentException('Shadupayouface!');
-        }
-        $this->contentObject = $contentObject;
-        return $this;
+        $this->setWorkingDir();
+        $this->setCharSet();
+        $this->setTypoScript();
+        $this->setContentObject($data, $table);
     }
 
     /**
@@ -94,10 +55,131 @@ class Tx_Smarty_Core_CobjectProxy
      * @return void
      * @see Tx_Extbase_Utility_FrontendSimulator::resetFrontendEnvironment
      */
-    public final function __destruct()
+    public function __destruct()
     {
-        if(!is_null($this->tsfeBackup)) {
+        $this->resetTsfe();
+        $this->resetWorkingDir();
+    }
+
+    /**
+     * Reroutes cObj method calls to the tslib_cObj instance
+     *
+     * @param $method
+     * @param array $args
+     * @return mixed
+     * @throws BadMethodCallException|RuntimeException
+     */
+    public function __call($method, array $args = array())
+    {
+        if (!($this->getTsfe() instanceof tslib_fe)) {
+            throw new RuntimeException('Couldn\'t get a valid instance of "tslib_fe"!', 1329912412);
+        } elseif(!method_exists($this->getTsfe()->cObj, $method)) {
+            throw new BadMethodCallException('No such method "' . $method . '" in class "tslib_cObj"!', 1329912359);
+        }
+        return call_user_func_array(array($this->getTsfe()->cObj, $method), $args);
+    }
+
+    /**
+     * @param array $data
+     * @param string $table
+     */
+    private function setContentObject(array $data = array(), $table = '')
+    {
+        $this->getTsfe()->cObj = t3lib_div::makeInstance('tslib_cObj');
+        $this->getTsfe()->cObj->start($data, $table);
+    }
+
+    /**
+     * Creates a dummy global instance of tslib_fe.
+     */
+    private function setTsfe()
+    {
+        $this->tsfeBackup = ($GLOBALS['TSFE'] instanceof tslib_fe) ? $GLOBALS['TSFE'] : false;
+        $GLOBALS['TSFE'] = new stdClass();
+        $GLOBALS['TSFE']->cObjectDepthCounter = 100;
+    }
+
+    /**
+     * Checks if the global tslib_fe instance has already been set. This is the case as soon
+     * as $tsfeBackup has been set (containing either a backup of the current tslib_fe global
+     * or the boolean "false").
+     *
+     * @return bool
+     */
+    private function hasTsfe()
+    {
+        return !is_null($this->tsfeBackup);
+    }
+
+    /**
+     * @return tslib_fe
+     */
+    private function getTsfe()
+    {
+        if (!$this->hasTsfe()) {
+            $this->setTsfe();
+        }
+        return $GLOBALS['TSFE'];
+    }
+
+    /**
+     *
+     */
+    private function setTypoScript()
+    {
+        $typoScriptSetup = array(); // TODO: Smarty setup
+        $template = t3lib_div::makeInstance('t3lib_TStemplate');
+        $template->tt_track = 0;
+        $template->init();
+        $template->getFileName_backPath = PATH_site;
+        $this->getTsfe()->tmpl = $template;
+        $this->getTsfe()->tmpl->setup = $typoScriptSetup;
+        $this->getTsfe()->config = $typoScriptSetup;
+    }
+
+    /**
+     *
+     */
+    private function setCharSet()
+    {
+        // preparing csConvObj
+        if (!is_object($this->getTsfe()->csConvObj)) {
+            if (is_object($GLOBALS['LANG'])) {
+                $this->getTsfe()->csConvObj = $GLOBALS['LANG']->csConvObj;
+            } else {
+                $this->getTsfe()->csConvObj = t3lib_div::makeInstance('t3lib_cs');
+            }
+        }
+
+        // preparing renderCharset
+        if (!is_object($this->getTsfe()->renderCharset)) {
+            if (is_object($GLOBALS['LANG'])) {
+                $this->getTsfe()->renderCharset = $GLOBALS['LANG']->charSet;
+            } else {
+                $this->getTsfe()->renderCharset = $GLOBALS['TYPO3_CONF_VARS']['BE']['forceCharset'];
+            }
+        }
+    }
+
+    private function setWorkingDir()
+    {
+        $this->workingDirBackup = getcwd();
+        chdir(PATH_site);
+    }
+
+    private function resetWorkingDir()
+    {
+        if (!is_null($this->workingDirBackup)) {
+            chdir($this->workingDirBackup);
+        }
+    }
+
+    private function resetTsfe()
+    {
+        if ($this->tsfeBackup instanceof tslib_fe) {
             $GLOBALS['TSFE'] = $this->tsfeBackup;
+        } else {
+            unset($GLOBALS['TSFE']);
         }
     }
 }
