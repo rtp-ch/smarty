02ed8b33ccfcc85222a69e8b3cda60c19fd54c3e
Christian Weiske <christian.weiske@netresearch.de>
2012-08-02 11:05:22 +0200

    Fix smarty bug #1: extension configuration options not respected, https://github.com/rtp-ch/smarty/issues/1

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,7 @@
 	$_EXTCONF = unserialize($_EXTCONF);
 
 	// Set global extension configuration vars
+	$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY] = $_EXTCONF;
 	$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['smarty_dir'] = preg_replace('%[\\\\|/]$%m', '', $_EXTCONF['smarty_dir']).'/';
 	$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['template_dir'] = t3lib_div::getFileAbsFileName(preg_replace('%[\\\\|/]$%m', '', $_EXTCONF['template_dir']));
 
