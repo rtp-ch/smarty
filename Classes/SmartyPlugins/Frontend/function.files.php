518d8eb4266c7312226d72e841315c63326af2f6
Simon Tuck <stu@rtp.ch>
2014-08-07 11:20:15 +0200

    Adds documentation for files plugin

--- Classes/SmartyPlugins/Frontend/function.files.php
+++ Classes/SmartyPlugins/Frontend/function.files.php
@@ -3,30 +3,23 @@
 /**
  * Smarty plugin "files"
  * -------------------------------------------------------------
- * File:    function.image.php
+ * File:    function.files.php
  * Type:    function
- * Name:    Image
- * Version: 2.0
+ * Name:    Files
+ * Version: 1.0
  * Author:  Simon Tuck <stu@rtp.ch>, Rueegg Tuck Partner GmbH
- * Purpose: Gets a TYPO3 content object type "IMAGE"
- * Example: {image file="fileadmin/pic.jpg" file.width="150" }
- * Example: {image setup="lib.myImage" file.width="150c" file.height="180c"}
- * Note:    You can add any property valid for the TypoScript IMAGE object,
- *          For example, file="fileadmin/pic.jpg" or width="150" or file.height="180c" etc.
- *          For details of available parameters check the IMAGE, imgResource and GIFBUILDER documentation at:
- *          http://typo3.org/documentation/document-library/references/doc_core_tsref/current/view/8/6/
- *          http://typo3.org/documentation/document-library/references/doc_core_tsref/current/view/5/2/
- *          http://typo3.org/documentation/document-library/references/doc_core_tsref/current/view/8/6/
- * Note:    The parameter "setup" will reference the global template scope, so you can pass a typoscript
- *          object which defines your link configuratiopn.
- *          For example, if your TypoScript setup contains an element lib.myImage, adding
- *          setup="lib.myImage" will use the TypoScript configuration from lib.myImage to create the image
- * Note:    Any parameters inside the image tag will override the corresponding parameters in the TypoScript
- *          object from setup. For example, {image setup="lib.myImage" file.width="150c"}
- *          will use the TypoScript configuration from lib.myImage, but set the property 'file.width' to 150c
+ * Purpose: Gets a TYPO3 content object type "FILES"
+ * Example: {files files=10 renderObj=TEXT stdWrap.data="file:current:title"}
+ *          Gets the title of the file with id 10 in sys_file
+ * Example: {files references=1 renderObj=TEXT stdWrap.data="file:current:title"}
+ *          Gets the title of the file referenced by id in sys_file_reference
+ * Note:    You can add any property valid for the TypoScript FILES object,
+ *          For details of available parameters check the FILES and FAL documentation at:
+ *          http://docs.typo3.org/typo3cms/TyposcriptReference/ContentObjects/Files/Index.html
+ *          http://wiki.typo3.org/File_Abstraction_Layer
  * -------------------------------------------------------------
  *
- * @param array $params The TypoScript settings passed to the image function
+ * @param array $params The TypoScript settings passed to the files function
  * @param Smarty_Internal_Template $template Current instance of smarty
  * @return string
  * @SuppressWarnings(PHPMD.UnusedFormalParameter)
