abedad3692183b3f4de0338f2fe4fecb1d075ae8
Simon Tuck <stu@rtp.ch>
2014-09-06 18:36:35 +0200

    Adds resource view helper

---
+++ Classes/SmartyPlugins/Common/function.resource.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * Smarty plugin "resource"
+ * -------------------------------------------------------------
+ * File:    function.resource.php
+ * Type:    function
+ * Name:    File resource
+ * Version: 1.0
+ * Author:    Simon Tuck <stu@rtp.ch>, Rueegg Tuck Partner GmbH
+ * Purpose: returns a FAL resource object or a property of the resource object
+ * Example:    {resource reference="12" property="publicUrl" assign="url"}
+ * -------------------------------------------------------------
+ *
+ * @param $params
+ * @param Smarty_Internal_Template $template
+ * @throws Tx_Smarty_Exception_PluginException
+ * @return mixed
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
+//@codingStandardsIgnoreStart
+function smarty_function_resource($params, Smarty_Internal_Template $template)
+{
+//@codingStandardsIgnoreEnd
+
+    $params = array_change_key_case($params, CASE_LOWER);
+
+    $fileReference = isset($params['reference']) ? $params['reference'] : false;
+    $requestedProperty = isset($params['property']) ? $params['property'] : false;
+
+    $resourceObject = null;
+    $resourceProperty = null;
+
+    if ($fileReference !== false) {
+        try {
+            if (\TYPO3\CMS\Core\Utility\MathUtility::canBeInterpretedAsInteger($fileReference)) {
+                /** @var TYPO3\CMS\Core\Resource\FileReference $resourceObject */
+                $resourceObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->getFileReferenceObject(
+                    $fileReference
+                );
+
+            } else {
+                // We have a combined identifier or legacy (storage 0) path
+                /** @var \TYPO3\CMS\Core\Resource\FileInterface|\TYPO3\CMS\Core\Resource\Folder $fileObject */
+                $resourceObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->retrieveFileOrFolderObject(
+                    $fileReference
+                );
+            }
+
+            if ($resourceObject instanceof \TYPO3\CMS\Core\Resource\ResourceInterface) {
+                if ($requestedProperty !== false) {
+                    // All properties of the \TYPO3\CMS\Core\Resource\FileInterface are available here:
+                    switch ($requestedProperty) {
+                        case 'name':
+                            $resourceProperty = $resourceObject->getName();
+                            break;
+
+                        case 'uid':
+                            $resourceProperty = $resourceObject->getUid();
+                            break;
+
+                        case 'originalUid':
+                            if ($resourceObject instanceof \TYPO3\CMS\Core\Resource\FileReference) {
+                                /** @var \TYPO3\CMS\Core\Resource\File $originalFile */
+                                $originalFile = $resourceObject->getOriginalFile();
+                                $resourceProperty = $originalFile->getUid();
+                            } else {
+                                $resourceProperty = null;
+                            }
+                            break;
+
+                        case 'size':
+                            $resourceProperty = $resourceObject->getSize();
+                            break;
+
+                        case 'sha1':
+                            $resourceProperty = $resourceObject->getSha1();
+                            break;
+
+                        case 'extension':
+                            $resourceProperty = $resourceObject->getExtension();
+                            break;
+
+                        case 'mimetype':
+                            $resourceProperty = $resourceObject->getMimeType();
+                            break;
+
+                        case 'contents':
+                            $resourceProperty = $resourceObject->getContents();
+                            break;
+
+                        case 'publicUrl':
+                            $resourceProperty = $resourceObject->getPublicUrl();
+                            break;
+
+                        case 'localPath':
+                            $resourceProperty = $resourceObject->getForLocalProcessing();
+                            break;
+
+                        default:
+                            // Generic alternative here
+                            $resourceProperty = $resourceObject->getProperty($requestedProperty);
+                    }
+
+                }
+
+            } else {
+                $resourceObject = null;
+            }
+
+        } catch (\Exception $e) {
+            $resourceObject = null;
+        }
+    }
+
+    // Returns or assigns the result
+    if (isset($params['assign'])) {
+        $template->assign($params['assign'], ($requestedProperty !== false ? $resourceProperty : $resourceObject));
+
+    } else {
+        return $requestedProperty !== false ? $resourceProperty : $resourceObject;
+    }
+}
